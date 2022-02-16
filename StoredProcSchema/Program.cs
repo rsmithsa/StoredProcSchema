90d5748c2527dc4bb134fbb79f766c30eedd4da5
rsmithsa <richard@smith-sa.com>
2022-02-16 08:50:22 +0200

    Add script generation

--- StoredProcSchema/Program.cs
+++ StoredProcSchema/Program.cs
@@ -39,6 +39,10 @@ namespace StoredProcSchema
 
             var parameters = app.Option("-p|--param", "Stored proc parameters", CommandOptionType.MultipleValue);
 
+            var outputScript = app.Option("-os|--output-script", "Generate table, table type & insert proc scripts as output", CommandOptionType.NoValue);
+            var outputSchema = app.Option("-sc|--output-schema", "Output schema name to use.", CommandOptionType.SingleValue);
+            var outputName = app.Option("-n|--output-name", "Output name to use.", CommandOptionType.SingleValue);
+
             app.OnExecute(async () =>
             {
                 using (var db = new SqlConnection(connectionString.Value()))
@@ -57,8 +61,84 @@ namespace StoredProcSchema
                         {
                             var dt = result.GetSchemaTable();
 
-                            Console.WriteLine(string.Join("|", dt.Columns.OfType<DataColumn>().Select(x => x.ColumnName)));
-                            Console.WriteLine(string.Join(Environment.NewLine, dt.Rows.OfType<DataRow>().Select(x => string.Join("|", x.ItemArray))));
+                            if (outputScript.HasValue())
+                            {
+                                Console.WriteLine($"DROP PROCEDURE IF EXISTS [{outputSchema.Value()}].[Load{outputName.Value()}]");
+                                Console.WriteLine($"DROP TABLE IF EXISTS [{outputSchema.Value()}].[{outputName.Value()}]");
+                                Console.WriteLine($"DROP TYPE IF EXISTS [{outputSchema.Value()}].[{outputName.Value()}Type]");
+                                Console.WriteLine();
+
+                                Console.WriteLine($"CREATE TABLE [{outputSchema.Value()}].[{outputName.Value()}] (");
+
+                                foreach (DataRow row in dt.Rows)
+                                {
+                                    var scaleInfo = row["DataType"].Equals(typeof(string)) ? $"({(row["ColumnSize"].Equals(int.MaxValue) ? "max" : row["ColumnSize"])})" : string.Empty;
+                                    Console.WriteLine($"    [{row["ColumnName"]}] [{row["DataTypeName"]}]{scaleInfo} {(row["AllowDBNull"].Equals(true) ? string.Empty : "NOT ")}NULL,");
+                                }
+
+                                Console.WriteLine($"    [DateInserted] [datetime] NOT NULL");
+                                Console.WriteLine($")");
+
+                                Console.WriteLine();
+
+                                Console.WriteLine($"CREATE TYPE [{outputSchema.Value()}].[{outputName.Value()}Type] AS TABLE (");
+
+                                bool first = true;
+                                foreach (DataRow row in dt.Rows)
+                                {
+                                    if (!first)
+                                    {
+                                        Console.WriteLine(",");
+                                    }
+
+                                    first = false;
+
+                                    var scaleInfo = row["DataType"].Equals(typeof(string)) ? $"({(row["ColumnSize"].Equals(int.MaxValue) ? "max" : row["ColumnSize"])})" : string.Empty;
+                                    Console.Write($"    [{row["ColumnName"]}] [{row["DataTypeName"]}]{scaleInfo} {(row["AllowDBNull"].Equals(true) ? string.Empty : "NOT ")}NULL");
+                                }
+
+                                Console.WriteLine();
+                                Console.WriteLine($")");
+
+                                Console.WriteLine();
+                                Console.WriteLine(
+@"SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+");
+                                Console.WriteLine($"CREATE PROCEDURE [{outputSchema.Value()}].[Load{outputName.Value()}]");
+                                Console.WriteLine($"@Data [{outputSchema.Value()}].[{outputName.Value()}Type] READONLY");
+
+                                Console.WriteLine(
+@"AS
+BEGIN");
+
+                                //Console.WriteLine($"    TRUNCATE TABLE [{outputSchema.Value()}].[{outputName.Value()}]");
+                                Console.WriteLine($"    INSERT INTO [{outputSchema.Value()}].[{outputName.Value()}] (");
+
+                                Console.Write(string.Join("," + Environment.NewLine, dt.Rows.OfType<DataRow>().Select(x => $"        [{x["ColumnName"]}]")));
+                                Console.WriteLine(",");
+                                Console.WriteLine("        [DateInserted]");
+
+                                Console.WriteLine($"    )");
+
+                                Console.WriteLine($"    SELECT");
+
+                                Console.Write(string.Join("," + Environment.NewLine, dt.Rows.OfType<DataRow>().Select(x => $"        [{x["ColumnName"]}]")));
+                                Console.WriteLine(",");
+                                Console.WriteLine("        GETDATE()");
+
+                                Console.WriteLine($"    FROM @Data;");
+
+                                Console.WriteLine($"END;");
+                            }
+                            else
+                            {
+                                Console.WriteLine(string.Join("|", dt.Columns.OfType<DataColumn>().Select(x => x.ColumnName)));
+                                Console.WriteLine(string.Join(Environment.NewLine, dt.Rows.OfType<DataRow>().Select(x => string.Join("|", x.ItemArray))));
+                            }
                         }
                     }
                 }
